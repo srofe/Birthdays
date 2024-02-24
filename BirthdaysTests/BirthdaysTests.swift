ccab1786c28d16d784cced95a0e0e2316a19a6c6
Simon Rofe <simon.rofe@ieee.org>
2024-02-25 00:58:58 +1000

    Refactor initial test
    Tests should run on the Main Actor (refer Jon Reid live CI/CD SwiftUI
    examples). Also, tests can throw exceptions (though this one doesn't).

--- BirthdaysTests/BirthdaysTests.swift
+++ BirthdaysTests/BirthdaysTests.swift
@@ -1,14 +1,8 @@
-//
-//  BirthdaysTests.swift
-//  BirthdaysTests
-//
-//  Created by Simon Rofe on 24/2/2024.
-//
-
 import XCTest
 
+@MainActor
 final class BirthdaysTests: XCTestCase {
-    func testZero() {
+    func testZero() throws {
         XCTAssertTrue(true)
     }
 }
