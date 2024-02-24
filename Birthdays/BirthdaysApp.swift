4857b46f020dbd0211ee64e55655d2f867eb0823
Simon Rofe <simon.rofe@ieee.org>
2024-02-24 23:00:48 +1000

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Simon Rofe on 24/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
