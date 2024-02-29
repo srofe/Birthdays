af2b1a321b4482bba383126f8090c795b6665b4f
Simon Rofe <simon.rofe@airbus.com>
2024-03-01 09:30:46 +1000

    Provide Contact with a last name

--- BirthdaysTests/ContactTests.swift
+++ BirthdaysTests/ContactTests.swift
@@ -2,11 +2,17 @@ import XCTest
 
 struct Contact {
     let firstName: String
+    let lastName: String
 }
 
 final class ContactTests: XCTestCase {
     func testContact_hasFirstName() throws {
-        let contact = Contact(firstName: "First")
+        let contact = Contact(firstName: "First", lastName: "")
         XCTAssertEqual(contact.firstName, "First", "A Contact shall have a first name.")
+    }
+
+    func testContact_hasLastName() throws {
+        let contact = Contact(firstName: "First", lastName: "Last")
+        XCTAssertEqual(contact.lastName, "Last")
     }
 }
