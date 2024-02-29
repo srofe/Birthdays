082014dd3bfe93f935211b5b2196292f3334b52a
Simon Rofe <simon.rofe@airbus.com>
2024-03-01 09:44:40 +1000

    Provide Birthday with a month

--- BirthdaysTests/BirthdayTests.swift
+++ BirthdaysTests/BirthdayTests.swift
@@ -2,11 +2,17 @@ import XCTest
 
 struct Birthday {
     let day: Int
+    let month: Int
 }
 
 final class BirthdayTests: XCTestCase {
-    func testExample() throws {
-        let birthday = Birthday(day: 24)
+    func testBirthday_hasDay() throws {
+        let birthday = Birthday(day: 24, month: 7)
         XCTAssertEqual(birthday.day, 24, "A Birthday shall have a day property.")
+    }
+
+    func testBirthday_hasMonth() throws {
+        let birthday = Birthday(day: 24, month: 7)
+        XCTAssertEqual(birthday.month, 7, "A Birthday shall have a month property.")
     }
 }
